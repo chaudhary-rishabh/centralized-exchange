790746a413e4d3009b9828ecb438dbfb49ba1231
chaudhary-rishabh <chaudharyrishabh029@gmail.com>
2026-05-28 08:18:19 +0530

    chore: init index and route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,41 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+import {prisma} from "@perps/db";
+import authRoute from "./module/auth/auth.route.js"
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 5000;
+
+
+app.use("/health", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: true,
+        messaeg: "server is up and running!"
+    })
+})
+
+app.use("/health/db", async(req: Request, res: Response) => {
+    try {
+        const response = await prisma.market.findFirst();
+        if (!response) {
+            res.status(404).json({
+                status: false,
+                message: "db not found"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            message: "Db connected and running"
+        })
+    } catch (error: unknown) {
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong"
+        })
+    }
+})
+
+app.use("/perps/v2/auth", authRoute)
 
 app.listen(PORT, () => {
-    console.log(`port listening on port ${5000}`)
+    console.log(`Server listening on port ${PORT}`)
 })
